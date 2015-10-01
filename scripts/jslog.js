d309586866244b1df52636c25d2e7f684edd725a
therin_a <therin_a@git.epitech.eu>
2015-10-01 11:02:42 +0200

    step-2

--- scripts/jslog.js
+++ scripts/jslog.js
@@ -1,33 +1,56 @@
 /**
  * Created by alexandre on 28/09/15.
  */
+(function(window, undefined){
 
-(function(window,undefined){
+    function SetErrorlog(){
 
-    window.server = undefined;
+        /* check the existance of an other window.onerror */
 
-    window.onerror = function(errMessage, errURL, line, col){
-        try {
-            var httpRequest = false;
-            var sending = 'Err : ' + errMessage + ' at ' + errURL + ' in line ' + line +
-                ':' + col;
-
-            if (window.XMLHttpRequest){
-                httpRequest = new XMLHttpRequest();
-            }
-            else if (window.ActiveXObject){
-                httpRequest = new ActiveXObject("Microsot.XMLHTTP");
-            }
-            if(!httpRequest){
-                console.log("Erreur impossible de créer une requête!");
-                return false;
-            }
-            httpRequest.open('POST', window.server, true);
-            httpRequest.setRequestHeader('Content-type', 'application/x-form-urlencoded');
-            httpRequest.send(sending);
-        } catch ( logginError) {
-            console.log( "Error logging failed" );
+        if (window.onerror){
+            console.log("Error window.onerror ever setting");
         }
-        return false;
-    };
-}(window));
+
+        /* setup params */
+        that = this;
+
+        this.param = {
+            url : "http://localhost:80",
+            loaded : true
+        };
+
+        /* definition of window.onerror */
+        window.onerror = function(errMessage, errURL, line, col){
+            try {
+                var httpRequest = false;
+                var sending = 'Err : ' + errMessage + ' at ' + errURL + ' in line ' + line +
+                    ':' + col;
+
+
+                /* set a new request */
+
+                if (window.XMLHttpRequest){ // for all browser except IE
+                    httpRequest = new XMLHttpRequest();
+                }
+                else if (window.ActiveXObject){// IE exception
+                    httpRequest = new ActiveXObject("Microsot.XMLHTTP");
+                }
+
+                if(!httpRequest){ //if not able to create a request
+                    console.log("Error failed to create new request");
+                    return false;
+                }
+
+                /* send the request */
+                httpRequest.open('POST', that.param.url, true);
+                httpRequest.setRequestHeader('Content-type', 'application/x-form-urlencoded');
+                httpRequest.send(sending);
+            } catch ( logginError) {
+                console.log( "Error logging failed" );
+            }
+            return false;
+        };
+        return this;
+    }
+    window.settingsErrorlog = new SetErrorlog();
+})(window);
