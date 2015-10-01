e4c4fe11a966c9ece9d4401caa74e1fbfe26dc70
therin_a <therin_a@git.epitech.eu>
2015-10-01 11:53:09 +0200

    step2-1

--- test/jslog.test.js
+++ test/jslog.test.js
@@ -2,9 +2,14 @@
  * Created by alexandre on 30/09/15.
  */
 
-
 describe("A suite", function() {
     it("contains spec with an expectation", function() {
-        expect(false).toBe(true);
+        expect(true).toBe(true);
     });
+});
+
+describe("Testing script load", function(){
+    it("Must be true", function () {
+        expect(onerror).toBe(null);
+    })
 });
