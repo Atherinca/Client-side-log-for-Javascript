02c56cf3fe35ba3df4eeb0f94d425eb55330934c
therin_a <therin_a@git.epitech.eu>
2015-10-09 15:18:50 +0200

    travis

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Client-side-log-for-Javascript
+[![Build Status](https://travis-ci.org/Atherinca/Client-side-log-for-Javascript.svg?branch=master)](https://travis-ci.org/Atherinca/Client-side-log-for-Javascript)
--- test/jslog.enabled.test.js
+++ test/jslog.enabled.test.js
@@ -10,18 +10,18 @@ describe("Our error log API, when enabled", function () {
       expect(method).toBe('POST');
       expect(typeof url).toBe('string');
       expect(async).toBe(true);
-      console.log("open success!")
+      console.log("open success!");
     };
 
     window.XMLHttpRequest.prototype.setRequestHeader = function (header, value) {
       expect(header).toBe('Content-type');
       expect(value).toBe('application/x-form-urlencoded');
-      console.log("setRequestHeader success!")
+      console.log('setRequestHeader success!');
     };
 
     window.XMLHttpRequest.prototype.send = function (string) {
       expect(typeof string).toBe('string');
-      console.log("send success!")
+      console.log("send success!");
     };
 
     window.onerror('Error message', 'URL of the script where the error was raised', 0, 0, {});
